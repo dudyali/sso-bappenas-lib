ec7aedbcfd52360eaa244cca37a31dbde279cc7c
Dudy Ali <dudyali@gmail.com>
2023-12-04 22:34:59 +0700

    add mockup function for getUser, check, and logout

--- src/SSO.php
+++ src/SSO.php
@@ -8,10 +8,25 @@ define('API_ENDPOINT', '/login');
 
 class SSO 
 {
-    public static function redirect()
+    public static function authenticate()
     {
         redirect(SERVER_HOST.API_ENDPOINT)->send();
         exit();
+    }
+
+    public static function getUser()
+    {
+        return "on development";
+    }
+
+    public static function check()
+    {
+        return "on development";
+    }
+
+    public static function logout()
+    {
+        return "on development";
     }
 }
 
